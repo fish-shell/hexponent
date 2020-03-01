3d17b4b840e2cd48d1fdeeb6fcd7ec8051c8f7e4
pythondude325 <me@gisch.dev>
2020-03-01 16:12:17 -0600

    Updated fuzzer options

--- fuzz/run_fuzzer.sh
+++ fuzz/run_fuzzer.sh
@@ -3,9 +3,10 @@ cd $(dirname "$0")
 
 HFUZZ_RUN_ARGS="\
 $HFUZZ_RUN_ARGS\
---threads=4\
---linux_perf_instr\
---linux_perf_branch\
---max_file_size=32"
+--threads=4 \
+--linux_perf_instr \
+--linux_perf_branch \
+--max_file_size=32 \
+--timeout=1"
 
 cargo hfuzz run fuzz --color=always
