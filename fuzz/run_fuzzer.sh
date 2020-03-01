e2eb1d0582150154faf371270e26505f090b724d
pythondude325 <me@gisch.dev>
2020-03-01 13:56:07 -0600

    Add fuzzing for doubles

--- fuzz/run_fuzzer.sh
+++ fuzz/run_fuzzer.sh
@@ -1,1 +1,8 @@
-HFUZZ_RUN_ARGS="--threads=4 --linux_perf_instr --linux_perf_branch --max_file_size=32" cargo hfuzz run fuzz --color=always
+#!/bin/sh
+HFUZZ_RUN_ARGS="\
+--threads=4\
+--linux_perf_instr\
+--linux_perf_branch\
+--max_file_size=32"
+
+cargo hfuzz run fuzz --color=always
--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -5,7 +5,8 @@ fn main() {
     loop {
         fuzz!(|data: &[u8]| {
             if let Ok(f) = FloatLiteral::from_bytes(data) {
-                assert!(!f.convert::<f32>().inner().is_nan());
+                assert!(!f.clone().convert::<f32>().inner().is_nan());
+                assert!(!f.convert::<f64>().inner().is_nan());
             }
         });
     }
